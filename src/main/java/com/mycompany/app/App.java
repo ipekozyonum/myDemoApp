53e5a723db40d8c57b7b11a3d63fd828f29b660f
ipekozyonum <iozyonum@etu.edu.tr>
2020-02-08 01:32:53 +0300

    Getting ready for deployment to heroku

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,7 +1,18 @@
 package com.mycompany.app;
 
+import static spark.Spark.get;
+import static spark.Spark.port;
+import static spark.Spark.post;
+
 import java.io.*;
 import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
+
+
+import spark.ModelAndView;
+import spark.template.mustache.MustacheTemplateEngine;
+
 
 /**
  * Hello world!
@@ -10,6 +21,7 @@ import java.util.*;
 
 public class App 
 {
+    /*
     public static void main( String[] args )
     {
         //System.out.println( "Hello World!" );
@@ -29,6 +41,71 @@ public class App
         System.out.println(kar_carpim(arr1, arr2, a, b));
 
     }
+*/
+
+    public static void main(String[] args) {
+        port(getHerokuAssignedPort());
+
+        get("/", (req, res) -> "Hello, World");
+
+        post("/compute", (req, res) -> {
+            //System.out.println(req.queryParams("input1"));
+            //System.out.println(req.queryParams("input2"));
+
+            String input1 = req.queryParams("input1");
+            String input2 = req.queryParams("input2s");
+            java.util.Scanner sc1 = new java.util.Scanner(input1);
+            java.util.Scanner sc2 = new java.util.Scanner(input2);
+            sc1.useDelimiter("[;\r\n]+");
+            sc2.useDelimiter("[;\r\n]+");
+            java.util.ArrayList<Integer> inputList = new java.util.ArrayList<>();
+            java.util.ArrayList<Integer> inputList2 = new java.util.ArrayList<>();
+            while (sc1.hasNext())
+            {
+                int value = Integer.parseInt(sc1.next().replaceAll("\\s",""));
+                inputList.add(value);
+            }
+            System.out.println(inputList);
+            while (sc2.hasNext())
+            {
+                int value = Integer.parseInt(sc2.next().replaceAll("\\s",""));
+                inputList2.add(value);
+            }
+            System.out.println(inputList);
+
+
+
+
+            String input3 = req.queryParams("input3").replaceAll("\\s","");
+            String input4= req.queryParams("input4").replaceAll("\\s","");
+            int input3AsInt = Integer.parseInt(input3);
+            int input4AsInt = Integer.parseInt(input4);
+
+            boolean result = App.kar_carpim(inputList,inputList2, input3AsInt,input4AsInt);
+
+            Map map = new HashMap();
+            map.put("result", result);
+            return new ModelAndView(map, "compute.mustache");
+        }, new MustacheTemplateEngine());
+
+
+        get("/compute",
+                (rq, rs) -> {
+                    Map map = new HashMap();
+                    map.put("result", "not computed yet!");
+                    return new ModelAndView(map, "compute.mustache");
+                },
+                new MustacheTemplateEngine());
+    }
+
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
+
 
     static boolean kar_carpim(ArrayList<Integer> arr1, ArrayList<Integer> arr2,int a, int b) {
         String istenen = "{" + a + "," + b + "}";
